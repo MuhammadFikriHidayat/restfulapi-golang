93532c914f239db1e2b5cd5ab946d0e64f17ae00
Muhammad Fikri <88938446+MuhammadFikriHidayat@users.noreply.github.com>
2023-01-10 21:10:34 +0700

    memperbaiki set awal project

--- back-end/config/db.go
+++ back-end/config/db.go
@@ -12,7 +12,7 @@ var DB *gorm.DB
 func InitDB() {
 	var err error
 
-	DB, err = gorm.Open(mysql.Open("root:@tcp(127.0.0.1)/log_barang?charset=utf8mb4&parseTime=True&loc=Local"), &gorm.Config{})
+	DB, err = gorm.Open(mysql.Open("root:@tcp(127.0.0.1)/apigo?charset=utf8mb4&parseTime=True&loc=Local"), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
