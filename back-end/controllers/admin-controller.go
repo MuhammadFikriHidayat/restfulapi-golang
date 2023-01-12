dc212ff7e5799bfe263d69e9ace22be36ee97881
Muhammad Fikri <88938446+MuhammadFikriHidayat@users.noreply.github.com>
2023-01-12 18:07:08 +0700

    add function get admin by id and function create admin (not yet tested)

--- back-end/controllers/admin-controller.go
+++ back-end/controllers/admin-controller.go
@@ -23,3 +23,36 @@ func GetAllAdmin(c echo.Context) error {
 		"data":    admin,
 	})
 }
+
+func GetAdminById(c echo.Context) error {
+	admin := model.Admin{}
+
+	c.Bind(&admin)
+	err := config.DB.Where("id = ? ", admin.Id).First(&admin).Error 
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, map[string]interface{}{
+			"message": admin.Id,
+		})
+	}
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "success",
+		"data":admin,
+	})
+}
+
+func CreateAdmin(c echo.Context) error {
+	createAdmin := model.Admin{}
+
+	c.Bind(&createAdmin)
+
+	err := config.DB.Save(&createAdmin).Error
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, map[string]interface{}{
+			"message": err.Error(),
+		})
+	}
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "success creating admin",
+		"data": createAdmin,
+	})
+}
